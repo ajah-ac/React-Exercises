616b8737d945f130fdb6cb51c20d2ca9426bdec7
ajah <ajahajang00@gmail.com>
2026-02-16 20:19:42 +0200

    Initial commit

---
+++ Exercise2/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  const [animal, setAnimal] = useState()
+
+
+
+
+function handleClick(){
+  return alert('Button was clicked')
+}
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
