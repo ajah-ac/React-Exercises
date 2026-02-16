e7bb64b4bd82dd49d5ccb514a435c8b7d5ff1b84
ajah <ajahajang00@gmail.com>
2026-02-16 22:49:25 +0200

    Css update

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -7,6 +7,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
+    <div className='displayArea'> here goes the solution</div>
     <main >
       <div className='leftside'>
      <button  >AC </button>
@@ -42,6 +44,7 @@ function App() {
 
 
     </main>
+    </>
   )
 }
 
