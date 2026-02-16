f8c04e69e3ac0891455d5d99d7df3f39d191d465
ajah <ajahajang00@gmail.com>
2026-02-16 22:38:18 +0200

    Css setup

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -7,9 +7,41 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     
-    </>
+    <main >
+      <div className='leftside'>
+     <button  >AC </button>
+     <button  >+/- </button>
+    <button  >%</button>
+         <button value='9' >9</button>
+                  <button value='8' >8</button>
+                  <button value='7' >7</button>
+                  <button value='6' >6</button>
+                  <button value='5' >5</button>
+                  <button value='4' >4</button>
+                  <button value='3' >3</button>
+                  <button value='2' >2</button>
+                  <button value='1' >1</button>
+         <button value='0' id='zero'>0</button>
+                  <button value='.' id='dot'>.</button>
+           
+
+
+
+</div>
+<div className='rightside'>
+<button>  ÷ </button>
+<button>  X </button>
+<button>  - </button>
+<button> + </button>
+<button>  = </button>
+
+
+
+</div>
+
+
+
+    </main>
   )
 }
 
