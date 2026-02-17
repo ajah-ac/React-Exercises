7ef8fcf6570749ccbfff0d6923674103b2a2581d
ajah <ajahajang00@gmail.com>
2026-02-17 14:46:07 +0200

    Two number calculation

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -4,46 +4,82 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [display, setDisplay] = useState('')
+  const [storedval,setStored]=useState(null)
+  const [operator, setOperator]=useState(null)
+  function handleNumber(val) {
+    if (val == 'ax') { setDisplay('') }
+    else if (val === '.' || !isNaN(val)) {
+      setDisplay(prev => prev + val)
+    }
+  }
+  function handleOperator(ops){
+setStored(Number(display))
+setDisplay('')
+setOperator(ops)
+  
+}
+function handleEquals(){
+  if(display==='' || storedval===null)return 
+  let result;
+  if(operator==='+')result=storedval + Number(display)
+  else if(operator==='-')result=storedval -Number(display)
+else if(operator==='x')result=storedval * Number(display)
+else if(operator==='/')result=storedval / Number(display)
+
+  setDisplay(result.toString())
+  setStored(null)
+  setOperator(null)
+}
+
+
+
+
+
+
+
+
+
+
 
   return (
     <>
-    <div className='displayArea'> here goes the solution</div>
-    <main >
-      <div className='leftside'>
-     <button  >AC </button>
-     <button  >+/- </button>
-    <button  >%</button>
-         <button value='9' >9</button>
-                  <button value='8' >8</button>
-                  <button value='7' >7</button>
-                  <button value='6' >6</button>
-                  <button value='5' >5</button>
-                  <button value='4' >4</button>
-                  <button value='3' >3</button>
-                  <button value='2' >2</button>
-                  <button value='1' >1</button>
-         <button value='0' id='zero'>0</button>
-                  <button value='.' id='dot'>.</button>
-           
+      <div className='displayArea'> {display}</div>
+      <main >
+        <div className='leftside'>
+          <button onClick={() => { handleNumber('ax') }} >AC </button>
+          <button >+/- </button>
+          <button onClick={() => { handleNumber('%') }}>%</button>
+          <button onClick={() => { handleNumber('9') }} >9</button>
+          <button onClick={() => { handleNumber('8') }}  >8</button>
+          <button onClick={() => { handleNumber('7') }}  >7</button>
+          <button onClick={() => { handleNumber('6') }}   >6</button>
+          <button onClick={() => { handleNumber('5') }}  >5</button>
+          <button onClick={() => { handleNumber('4') }}  >4</button>
+          <button onClick={() => { handleNumber('3') }}  >3</button>
+          <button onClick={() => { handleNumber('2') }}  >2</button>
+          <button onClick={() => { handleNumber('1') }}  >1</button>
+          <button onClick={() => { handleNumber('0') }} id='zero'>0</button>
+          <button onClick={() => { handleNumber('.') }} id='dot'>.</button>
 
 
 
-</div>
-<div className='rightside'>
-<button>  ÷ </button>
-<button>  X </button>
-<button>  - </button>
-<button> + </button>
-<button>  = </button>
+
+        </div>
+        <div className='rightside'>
+          <button onClick={() => { handleOperator('/') }}>  ÷ </button>
+          <button onClick={() => { handleOperator('x') }}>  X </button>
+          <button onClick={() => { handleOperator('-') }}>  - </button>
+          <button onClick={() => { handleOperator('+') }}> + </button>
+          <button onClick={ handleEquals}> = </button>
 
 
 
-</div>
+        </div>
 
 
 
-    </main>
+      </main>
     </>
   )
 }
