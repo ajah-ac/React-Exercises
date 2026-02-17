028b4bec3d99fe3e70b984e733bb2f1577c420f2
ajah <ajahajang00@gmail.com>
2026-02-17 18:26:56 +0200

    Modify logic

--- QuizApplication/src/App.jsx
+++ QuizApplication/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 import Quiz from './quiz'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <Quiz/>
-  
     </>
   )
 }
