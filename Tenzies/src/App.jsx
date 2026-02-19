a4e4641c1babcdaa2aa116924110bde84cd7e971
ajah <ajahajang00@gmail.com>
2026-02-19 16:00:24 +0200

    Add Die component

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -2,14 +2,36 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Die from './Die'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    
-    </>
+    <main>
+      <div>
+
+
+    <h1>Tenzies</h1>
+    <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      </div>
+
+<div>
+
+
+  <Die value={1}/>
+  <Die value={2}/>
+  <Die value={3}/>
+  <Die value={4}/>
+  <Die value={5}/>
+  <Die value={6}/>
+  <Die value={1}/>
+  <Die value={2}/>
+  <Die value={3}/>
+  <Die value={4}/>
+
+
+</div>
+    </main>
   )
 }
 
---
+++ Tenzies/src/Die.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+export default function Die(props){
+    return (
+        <>
+        <button> {props.value} </button>
+        
+        </>
+    )
+}
