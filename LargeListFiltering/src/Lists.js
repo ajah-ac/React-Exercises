a12d568ce5600f2fc1ab41ac07b08d8c203347b8
ajah <ajahajang00@gmail.com>
2026-03-08 09:56:17 +0200

    Create 1000 dummy data

--- LargeListFiltering/src/Lists.js
+++ LargeListFiltering/src/Lists.js
@@ -1,4 +1,3 @@
- import React from 'react'
  
  const Lists = Array.from({lenght:1000},(_,i)=>({
 id:i+1,
@@ -6,6 +5,5 @@ user:`User ${i+1}`,
 age:Math.floor(Math.random()*60)+18,
 country:["Canada",'USA','Russia','Rwanda','Uganda','South Africa','Jamaica'][Math.floor(Math.random())*7]
  }))
- 
  
  export default Lists
