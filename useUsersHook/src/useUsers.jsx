d8d8d5f259aa23237c67ac44a8c01ad76ace2a37
ajah <ajahajang00@gmail.com>
2026-03-10 13:06:41 +0200

    create custom useUser Hook

--- useUsersHook/src/useUsers.jsx
+++ useUsersHook/src/useUsers.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { useEffect } from 'react'
 const useUsers = () => {
-  return (
-    <div>useUsers</div>
-  )
+    const [data,setData]=useState([])
+    const [isLoading,setIsLoading]=useState(true)
+    const [Error,setError]=useState(null)
+    useEffect(()=>{
+        setIsLoading(true)
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(res=>res.json())
+.then(resp=>setData(resp.slice(0,10)))
+.catch(error=>setError(error))
+.finally(()=>setIsLoading(false))
+
+    },[])
+  return {
+    users:data,
+    isLoading:isLoading,
+    error:Error
+  }
 }
 
 export default useUsers
