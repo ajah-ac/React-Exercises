e0400c7e133e9c20bc2224f90cf57cf134140167
ajah <ajahajang00@gmail.com>
2026-03-10 13:07:02 +0200

    Implement useUser hook

--- useUsersHook/src/App.jsx
+++ useUsersHook/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import useUsers from './useUsers'
 function App() {
-  const [count, setCount] = useState(0)
-
+const {isLoading,error,users}=useUsers()
   return (
     <>
-      
+      {isLoading && <h1>Please wait as we fetch your data</h1>}
+      {error!==null && <h1> An error occured while fetching your data</h1>}
+      {users.length> 0 && <ul>
+        {users.map(user=><li key={user.id}> <h1>Name:{user.name}</h1> </li>)}
+        
+        
+        </ul>}
     </>
   )
 }
